3576a123cade8175eaca08f75b22cd1a45edd182
Austin Perdue <47020337+austinperdue@users.noreply.github.com>
2025-01-04 00:00:37 -0500

    Create arrays-ds-easy.py

---
+++ challenges/arrays-ds-easy.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'reverseArray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+# INPUT FORMAT
+# The first line contains an integer n, the number of integers in array a.
+# The second line contains n space-separated integers that describe a.
+# OUTPUT
+# Print all n integers in a in reverse order as a single line of space-separated integers.
+
+def reverseArray(a):
+    
+    # Write your code here
+
+    # can also do return a[::-1] (slicing)
+    
+    # python reverse function (returns None)
+    a.reverse()
+    
+    # return reversed list
+    return a
+    
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    fptr = sys.stdout
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
